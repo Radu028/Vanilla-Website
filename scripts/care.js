ac974e129b661e49ed3cb09f6e60e67895c5f3d6
Radu <radupopa028@gmail.com>
2025-01-03 14:22:29 +0200

    Add calc food for each breed

--- scripts/care.js
+++ scripts/care.js
@@ -1,4 +1,4 @@
-function confirmData() {
+async function confirmData() {
   const breed = document.getElementById('cat-breeds').value;
   const weight = document.getElementById('cat-weight').value;
   const weightError = document.getElementById('weight-error');
@@ -7,9 +7,55 @@ function confirmData() {
     weightError.style.opacity = 1;
     return;
   }
-  
+
   weightError.style.opacity = 0;
 
-  console.log('Breed:', breed);
-  console.log('Weight:', weight);
+  const catInfo = document.getElementById('cat-info');
+  catInfo.style.display = 'block';
+
+  const catInfoContent = document.getElementById('cat-info-content');
+  catInfoContent.innerHTML = '';
+
+  try {
+    const foodInfo = await calculateFood(breed, weight);
+    console.log(foodInfo.caloricNeeds);
+    catInfoContent.innerHTML = `
+      <h4>Informații nutriționale zilnice</h4>
+      <p>Necesar caloric: ${foodInfo.caloricNeeds} kcal</p>
+      <p>Mâncare uscată: ${foodInfo.dryFood} g</p>
+      <p>Mâncare umedă: ${foodInfo.wetFood} g</p>
+    `;
+  } catch (error) {
+    console.error('Error:', error);
+    catInfoContent.textContent = 'A apărut o eroare la calcularea necesarului de hrană.';
+  }
 }
+
+const calculateFood = async (breed, weight) => {
+  return fetch('../jsons/care.json')
+    .then((response) => {
+      if (response.ok && response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error(`Statusul este: ${response.status}`);
+      }
+    })
+    .then((breeds) => {
+      if (!breeds[breed]) {
+        throw new Error(`Rasa ${breed} nu a fost gasita.`);
+      }
+
+      const activityFactor = breeds[breed].activityFactor;
+      const caloricNeeds = activityFactor * Math.pow(weight, 0.75);
+
+      const dryFood = (caloricNeeds / 400) * 100;
+      const wetFood = (caloricNeeds / 90) * 100;
+
+      return {
+        caloricNeeds: Math.round(caloricNeeds),
+        dryFood: Math.round(dryFood),
+        wetFood: Math.round(wetFood),
+      };
+    })
+    .catch((error) => console.error('Error:', error));
+};
